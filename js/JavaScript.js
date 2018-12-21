4198decd176a63a3f95c339bc0c0d2da624f279a
Mafros <43349854+Mafros@users.noreply.github.com>
2018-12-21 18:02:02 +0300

    File for site

---
+++ js/JavaScript.js
@@ -0,0 +1,10 @@
+function MsgPylot() {
+    alert("Ты уверен?");
+    location.href='./scrt/MsgPylot.html';
+}
+
+function ChatPylot() {
+    if(button == "Привет"){
+        document.write("Кто это?")
+    }
+}
